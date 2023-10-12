c14a78adb54e3fcc5b87c54e52817b1ebe90adfe
beratagun <berat.agun@gmail.com>
2023-10-12 17:58:14 +0300

    Minesweeper week3

---
+++ src/week3/Main.java
@@ -0,0 +1,23 @@
+package week3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Mayın tarlası oyununa hoşgeldiniz!");
+        System.out.println("Haritanın boyutunu belirleyin.");
+
+        //Dizi (matris) boyutu kullanıcıdan alıyoruz
+        System.out.print("Satır sayısını giriniz: ");
+        int row = input.nextInt();
+        System.out.print("Sütun sayısını giriniz: ");
+        int col = input.nextInt();
+        System.out.println("------------------------------------");
+
+        MineSweeper mine = new MineSweeper(row,col);
+        mine.run();
+    }
+
+}
